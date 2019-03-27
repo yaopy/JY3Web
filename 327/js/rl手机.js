cddc794ffc607b38c425631cc042ed3399711217
yaopy <48549025+yaopy@users.noreply.github.com>
2019-03-27 23:22:39 +0800

    Add files via upload

---
+++ 327/js/rl手机.js
@@ -0,0 +1,5 @@
+$("*").css({"margin":"0px","padding":"0px"});
+$("#q").append("<div id='qleft'></div><div id='qcenter'>华为专区</div><div id='qright'></div>");
+$("#qright").append("<table><tr><td>收&nbsp;藏&nbsp;&nbsp;||</td><td>935.3w<br/>粉丝</td></tr></table>")
+//$("#w").append("<div id='subw'></div>")
+$("#w").append("<img id='w1' src='../img/首页-选中.svg'/><img id='w2' src='../img/全部商品.svg' /><img id='w3' src='../img/上新.svg' />")
