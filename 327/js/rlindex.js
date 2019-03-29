368cea5acc12e6ba4fdf0deb493207439cb434a0
yaopy <48549025+yaopy@users.noreply.github.com>
2019-03-29 23:55:07 +0800

    Add files via upload

--- 327/js/rlindex.js
+++ 327/js/rlindex.js
@@ -8,8 +8,9 @@ $("#w").css({"width":"100%","height":"120px","display":"flex","justify-content":
 $("#w").append("<input type='text' placeholder='输入商品名' /> ");
 $("#e").append("<div id='ebody'></div>");
 $("#ebody").append("<div id='e1'></div><div id='e2'></div><div id='e3'></div><div id='e4'></div>");
-$("#r").append("<div id='r1'><a href='rl手机.html'>手机</a></div><div id='r2'>平板</div><div id='r3'>电脑</div><div id='r4'>配件</div>");
-$("#t").append("<div id='banner'></div>");	
+//$("#r").append("<div id='r1'><a href='rl手机.html'>手机</a></div><div id='r2'>平板</div><div id='r3'>电脑</div><div id='r4'>配件</div>");
+$("#r").append("<div id='r1'><a href='rl分类.html'>&nbsp;</a>");
+$("#t").append("<div id='banner'></div>");
 $("#y").append("<span>爆款推荐</span>");	
 $("#i").append("<span>限时秒杀</span>");	
 
---
+++ 327/js/rl分类.js
@@ -0,0 +1,3 @@
+$("#w").append("<img src='../img/O1CN01p4O1Ct1CmNE7Zfv1o_!!123-0-lubanu.jpg' />");
+$("#e").append("<img src='../img/O1CN01qYEOO71C4rbDw8SO1_!!28-0-lubanu.jpg' />");
+$("#r").append("<img src='../img/TB14ab1KpXXXXclXFXXSutbFXXX.jpg_q50.jpg' />");
